0ba48da8e05bec1154e20e2bb178f087e0aab6a1
leige02 <xianchengyechao520@126.com>
2020-07-27 17:21:01 +0800

    leige02修改

--- src/main/java/com/test/demo/FirstGitDemo.java
+++ src/main/java/com/test/demo/FirstGitDemo.java
@@ -2,6 +2,8 @@ package com.test.demo;
 
 public class FirstGitDemo {
     public static void main(String[] args) {
-        System.out.println("this is my firs time to use git!!");
+        System.out.println("aaaaaaaaaaaaaaaa");
+        System.out.println("bbbbbbbbbbbbbbb");
+        System.out.println("cccccccccccccc");
     }
 }
